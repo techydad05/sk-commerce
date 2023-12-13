69a93a5a67906f5574f9435e4d61e8ccefb7350d
Ivan <techydad05@gmail.com>
2023-12-13 17:40:45 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  server: {port:3001},
 };
 
 export default config;
